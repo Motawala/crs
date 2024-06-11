b5fe43b96c0de0de42c332334f446377d545e52b
Motawala <motawala@MotawalaPC.localdomain>
2024-06-10 21:51:18 -0400

    Input Fields for New Reservation

--- public/js/newReservation.js
+++ public/js/newReservation.js
@@ -31,8 +31,10 @@ function createNewReservationDiv(){
         closeIcon.addEventListener('click', () => {
             removeNewReservationDiv()
         });
-        newReservationDiv.appendChild(closeIcon)
+        
         roomContainer.appendChild(newReservationDiv)
+        createInputs()
+        newReservationDiv.appendChild(closeIcon)
     }else{
         console.log("New Reservation Div already displayed")
     }
@@ -57,4 +59,108 @@ function removeNewReservationDiv(){
     const newReservationDiv = document.getElementById('newReservation-div')
     const roomContainer = document.getElementById('room-container')
     roomContainer.removeChild(newReservationDiv);
+}
+
+function createInputs(){
+    const newReservationDiv = document.getElementById('newReservation-div');
+    const inputsDiv = document.createElement("div");
+    inputsDiv.style.position = "relative"
+    inputsDiv.style.width = "90%"
+    inputsDiv.style.height = "90%"
+    inputsDiv.style.alignSelf = "center"
+    inputsDiv.style.alignItems = "left"
+    inputsDiv.style.alignContent = "center"
+    inputsDiv.style.justifyContent = "center"
+    inputsDiv.style.display = "flex"
+    inputsDiv.style.flexDirection = "row"
+    inputsDiv.id = "newReservation-input-div"
+    inputsDiv.style.padding = "10px"
+    inputsDiv.style.margin = "5px"
+    inputsDiv.style.flexWrap = "wrap"
+    newReservationDiv.appendChild(inputsDiv)
+
+
+    let input_list = ["firstname", "middlename", "lastname", "Phone Number"]
+    input_list.forEach(input =>{
+        createInputFields(input)
+    })
+
+    createAddressInput()
+}
+
+function createInputFields(name){
+    const field = document.createElement('input')
+    const inputsDiv = document.getElementById("newReservation-input-div")
+    field.style.width = "28%"
+    field.style.height = "5%"
+    field.placeholder = name.toUpperCase() + "*"
+    field.style.border = "2px groove black"
+    field.style.backgroundColor = "whitesmoke"
+    field.style.color = "grey"
+    field.id = name + "-input"
+    field.style.margin = "5px"
+
+    inputsDiv.appendChild(field)
+}
+
+function createAddressInput(){
+    const inputsDiv = document.getElementById("newReservation-input-div")
+    const addressLine1 = document.createElement("input")
+    addressLine1.id = "address-line-1"
+    addressLine1.style.width = "90%"
+    addressLine1.style.height = "5%"
+    addressLine1.placeholder =  "ADDRESS LINE 1*"
+    addressLine1.style.border = "2px groove black"
+    addressLine1.style.backgroundColor = "whitesmoke"
+    addressLine1.style.color = "grey"
+    addressLine1.id =  "address-line-1-input"
+    addressLine1.style.margin = "5px"
+
+    const addressLine2 = document.createElement("input")
+    addressLine2.id = "address-line-2"
+    addressLine2.style.width = "90%"
+    addressLine2.style.height = "5%"
+    addressLine2.placeholder =  "ADDRESS LINE 2"
+    addressLine2.style.border = "2px groove black"
+    addressLine2.style.backgroundColor = "whitesmoke"
+    addressLine2.style.color = "grey"
+    addressLine2.id =  "address-line-2-input"
+    addressLine2.style.margin = "5px"
+
+    const city = document.createElement("input")
+    city.id = "city-input"
+    city.style.width = "28%"
+    city.style.height = "5%"
+    city.placeholder =  "CITY*"
+    city.style.border = "2px groove black"
+    city.style.backgroundColor = "whitesmoke"
+    city.style.color = "grey"
+    city.style.margin = "5px"
+
+    const state = document.createElement("input")
+    state.id = "state-input"
+    state.style.width = "28%"
+    state.style.height = "5%"
+    state.placeholder =  "STATE*"
+    state.style.border = "2px groove black"
+    state.style.backgroundColor = "whitesmoke"
+    state.style.color = "grey"
+    state.style.margin = "5px"
+
+    const zipcode = document.createElement("input")
+    zipcode.id = "zipcode-input"
+    zipcode.style.width = "28%"
+    zipcode.style.height = "5%"
+    zipcode.placeholder =  "ZIP-CODE*"
+    zipcode.style.border = "2px groove black"
+    zipcode.style.backgroundColor = "whitesmoke"
+    zipcode.style.color = "grey"
+    zipcode.style.margin = "5px"
+
+
+    inputsDiv.appendChild(addressLine1)
+    inputsDiv.appendChild(addressLine2)
+    inputsDiv.appendChild(city)
+    inputsDiv.appendChild(state)
+    inputsDiv.appendChild(zipcode)
 }
